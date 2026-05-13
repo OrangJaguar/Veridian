bb08804894c61c6e6ebdf5c8285ca894fb92c982
basketballer4236 <basketballer4236@outlook.com>
2026-05-13 16:51:39 +0000

    Manual code change

--- src/components/AxiomLayout.jsx
+++ src/components/AxiomLayout.jsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { runAxiomApp } from '../axiom/runAxiomApp';
 export default function AxiomLayout() {
-  const rootRef = useRef(null);
   useEffect(() => {
-  if (!rootRef.current) return;
-  const container = rootRef.current;
-  // Defer by one frame to guarantee the container is painted
-  const id = requestAnimationFrame(() => {
-    runAxiomApp(container);
-  });
-  return () => cancelAnimationFrame(id);
-}, []);
-  return <div ref={rootRef} id="axiom-root" style={{ width: '100%', height: '100vh' }} />;
+    runAxiomApp();
+  }, []);
+  return null;
 }
