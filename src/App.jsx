c78e5efba607c7605459bb91793d06a8abf59474
basketballer4236 <basketballer4236@outlook.com>
2026-05-13 16:47:59 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-import { useLayoutEffect } from 'react';
 import AxiomLayout from './components/AxiomLayout';
-import { runAxiomApp } from './axiom/runAxiomApp';
-
-let axiomBootStarted = false;
-
 export default function App() {
-  useLayoutEffect(() => {
-    if (axiomBootStarted) return;
-    axiomBootStarted = true;
-    runAxiomApp();
-  }, []);
-
   return <AxiomLayout />;
 }
