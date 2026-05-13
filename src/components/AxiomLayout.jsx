97a34405c05fae7c24a1aaae57a597a7a3b6b94f
basketballer4236 <basketballer4236@outlook.com>
2026-05-13 16:49:41 +0000

    Manual code change

--- src/components/AxiomLayout.jsx
+++ src/components/AxiomLayout.jsx
@@ -3,9 +3,13 @@ import { runAxiomApp } from '../axiom/runAxiomApp';
 export default function AxiomLayout() {
   const rootRef = useRef(null);
   useEffect(() => {
-    if (rootRef.current) {
-      runAxiomApp(rootRef.current);
-    }
-  }, []);
+  if (!rootRef.current) return;
+  const container = rootRef.current;
+  // Defer by one frame to guarantee the container is painted
+  const id = requestAnimationFrame(() => {
+    runAxiomApp(container);
+  });
+  return () => cancelAnimationFrame(id);
+}, []);
   return <div ref={rootRef} id="axiom-root" style={{ width: '100%', height: '100vh' }} />;
 }
